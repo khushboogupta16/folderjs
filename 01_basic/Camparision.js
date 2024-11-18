4de611d0154c2ac5cc18c79aba80d66939e3fcc0
khushboogupta16 <kuku160301@gmail.com>
2024-11-18 12:31:21 +0000

    camparision

---
+++ 01_basic/Camparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1); true
+// console.log(2 >= 1); true
+// console.log(2 < 1); false
+// console.log(2 == 1);false
+// console.log(2 != 1);true
+
+// Do not camparision between two difference datetypes always wirte clean code 
+
+// console.log("2" > 1);true
+// console.log("02" > 1);true
+// console.log( null > 0);false
+// console.log( null == 0);false
+// console.log( null >= 0);true
+// console.log(undefined == 0);false
+// console.log(undefined > 0);false
+// console.log(undefined < 0);false
+
+// === check equal to plus datatypes
+
+// console.log("2" === 2);false
+
+
+
+
